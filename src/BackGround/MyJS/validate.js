731c2612ad40eb9ace75ca481fa00e02aeb7d935
Lettino <fm518812@163.com>
2017-12-16 08:46:34 +0800

    2017/12/17

--- src/BackGround/MyJS/validate.js
+++ src/BackGround/MyJS/validate.js
@@ -1,12 +1,6 @@
 // 验证
-function Validate() {
-    var schoolID=parseInt(myForm.logSchool.value);//学校ID
-    var teacherID = myForm.logName.value;//教师ID
-    var teacherpass = myForm.logPass.value;//登陆密码
+function Validate(schoolID,teacherID,teacherpass) {
     console.log(schoolID+" "+teacherID+" "+teacherpass)
-    console.log(typeof schoolID);
-    console.log(typeof teacherID);
-    console.log(typeof teacherpass);
         $.ajax(
         {
             type:'GET',
@@ -20,9 +14,16 @@ function Validate() {
             success:function(data) {
                 if(JSON.stringify(data)!="false"){
                     alert("登陆成功");
-                    window.localStorage.clear();
-                    window.localStorage.setItem("teacherInfo",JSON.stringify(data));
+                    //cookiesb保存
+                    $.cookie('schoolID', schoolID, { expires: 7 ,path:'/'});
+                    $.cookie('teacherID', teacherID, { expires: 7 ,path:'/'});
+                    $.cookie('teacherpass', teacherpass, { expires: 7 ,path:'/'});
+                    window.sessionStorage.clear();
+                    window.sessionStorage.setItem("teacherInfo",JSON.stringify(data));
+                    //判断用户信息
+                    //如果是教师
                     window.location.href="../BackGround/dashboard.html"
+                    //如果是管理员
                 }else {
                     alert("检查账户名或密码是否正确");
                 }
@@ -37,96 +38,83 @@ function Validate() {
     );
 }
 
-// var teacherInfo={
-//     "school_id": 1,
-//     "teacher_id": "t_0001",
-//     "teacher_name": "张华",
-//     "class_ids": [
-//         1,
-//         2
-//     ],
-//     "classes": [
-//         {
-//             "class_id": 1,
-//             "class_name": "四年级一班"
-//         },
-//         {
-//             "class_id": 2,
-//             "class_name": "四年级二班"
-//         }
-//     ],
-//     "subject_ids": [
-//         1,
-//         2
-//     ],
-//     "subjects": [
-//         {
-//             "subject_id": 1,
-//             "subject_name": "数学"
-//         },
-//         {
-//             "subject_id": 2,
-//             "subject_name": "数学"
-//         }
-//     ],
-//     "class_subject_papers": [
-//         {
-//             "class_id": 1,
-//             "class_name": "四年级一班",
-//             "subjects": [
-//                 {
-//                     "class_id": 1,
-//                     "class_name": "四年级一班",
-//                     "subject_id": 1,
-//                     "subject_name": "数学",
-//                     "test_papers": [
-//                         {
-//                             "paper_id": 1,
-//                             "paper_name": "第一单元测试",
-//                             "test_time": "2017-06-30T16:00:00.000Z"
-//                         },
-//                         {
-//                             "paper_id": 2,
-//                             "paper_name": "第二单元测试",
-//                             "test_time": "2017-07-09T16:00:00.000Z"
-//                         },
-//                         {
-//                             "paper_id": 3,
-//                             "paper_name": "第三单元测试",
-//                             "test_time": "2017-07-19T16:00:00.000Z"
-//                         }
-//                     ]
-//                 }
-//             ]
-//         },
-//         {
-//             "class_id": 2,
-//             "class_name": "四年级二班",
-//             "subjects": [
-//                 {
-//                     "class_id": 2,
-//                     "class_name": "四年级二班",
-//                     "subject_id": 2,
-//                     "subject_name": "数学",
-//                     "test_papers": [
-//                         {
-//                             "paper_id": 1,
-//                             "paper_name": "第一单元测试",
-//                             "test_time": "2017-06-30T16:00:00.000Z"
-//                         },
-//                         {
-//                             "paper_id": 2,
-//                             "paper_name": "第二单元测试",
-//                             "test_time": "2017-07-09T16:00:00.000Z"
-//                         },
-//                         {
-//                             "paper_id": 3,
-//                             "paper_name": "第三单元测试",
-//                             "test_time": "2017-07-19T16:00:00.000Z"
-//                         }
-//                     ]
-//                 }
-//             ]
-//         }
-//     ]
-// }
+function Register() {
+    //判断密码是否一致
+    var teacherPass = $("#password").val()
+    var teacherPass2 = $("#password2").val()
+    if(checkStrEqual(teacherPass,teacherPass2)){
+        var schoolID = $("#schoolList").val()
+        var teacherID = $("#login_field").val()
+        var teacherName = $("#login_name").val()
+        var teacherPass = $("#password").val()
+        console.log(schoolID+" "+teacherID+" "+teacherName+" "+teacherPass)
+        console.log(typeof schoolID);
+        console.log(typeof teacherID);
+        console.log(typeof teacherPass);
+        $.ajax(
+            {
+                type :"GET",
+                url :"http://localhost:3003/register_mysql/",
+                dataType: 'jsonp',
+                data :{
+                    teacher_name : teacherName,
+                    account  	 : teacherID,
+                    passwd       : teacherPass,
+                    school_id    : schoolID
+                },
+                jsonp:"callback",
+                jsonpCallback:"callback",
+                success:function(data) {;
+                    if(data=="true"){
+                        alert("注册成功")
+                    }else {
+                        alert("账号已存在，注册失败");
+                    }
+                },
+                error:function() {
+                    alert("注册失败，请检查网络连接");
+                }
+            }
+        );
+    }else {
+        alert("您输入的两次密码不一致，请重新输入")
+
+    }
+}
+function getUserDate() {
+    var schoolID, teacherID,teacherpass;
+    schoolID = $.cookie('schoolID');
+    teacherID = $.cookie('teacherID');
+    teacherpass = $.cookie('teacherpass');
+    alert(schoolID);
+    $.ajax(
+        {
+            type:'GET',
+            url : 'http://localhost:3003/login_mysql/',
+            dataType : 'jsonp',
+            jsonp:"jsoncallback",
+            jsonpCallback:"callback",
+            async: false,
+            data:{
+                school_id : schoolID , account : teacherID , password : teacherpass
+            },
+            success:function(data) {
+                if(JSON.stringify(data)!="false"){
+                    window.sessionStorage.clear();
+                    window.sessionStorage.setItem("teacherInfo",JSON.stringify(data));
+                    //判断用户信息
+
+                    //如果是管理员
+                }else {
+                    alert("检查账户名或密码是否正确");
+                }
+            },
+            error:function() {
+                alert("登陆失败，请检查网络连接");
+                // window.localStorage.clear();
+                // window.localStorage.setItem("teacherInfo",JSON.stringify(teacherInfo));
+                // window.location.href="../BackGround/dashboard.html"
+            }
+        }
+    );
+}
